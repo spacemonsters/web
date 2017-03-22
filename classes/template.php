7cf15ef02c1d69a57f9993c861bae066217b02b9
Martti <martti.merilain@khk.ee>
2017-03-22 12:20:18 +0200

    Compiled application configuration first version

--- classes/template.php
+++ classes/template.php
@@ -7,6 +7,10 @@
  * Time: 12:11
  */
 class template
-{
-
-}
+{//klassi algus
+//template klassi omadused - muutujad
+    var $file = ""; //html malli faili nimi
+    var $content = false; //html faili sisu
+    var $vars = array(); //html vaate sisu - reaalsed väärtused
+}//klassi lõpp
+?>
