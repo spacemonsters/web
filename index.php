bd274164c71c5ea74141b154f8dd6aeb292cf570
Martti <martti.merilain@khk.ee>
2017-03-22 13:05:52 +0200

    katsetuseks

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 //võtame konfiguratsiooni kasutusele
 require_once "conf.php";
 //pealehe sisu
-echo "<h1>Hello fleshy mammals</h1>"
+echo "<h1>Hello fleshy mammals</h1>";
+//valmistame pea malli/template
+$main_tmpl = new template(TMPL_DIR."main.html");
+//koostatud objekti kontrollimine
+echo "<pre>";
+print_r($main_tmpl);
+echo "</pre>";
 ?>
