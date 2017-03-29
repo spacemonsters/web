3b860694a1064979214d297537bc8dcb5b25f7d9
Martti <martti.merilain@khk.ee>
2017-03-29 12:38:46 +0300

    sisuelemendid

--- conf.php
+++ conf.php
@@ -6,8 +6,7 @@
  * Time: 15:25
  */
 //defineerime vajalikud konstandid
-define("CLASSES_DIR", "classes/");//classes kataloogi nimeline konstant
-define("TMPL_DIR", "tmpl/");//tmpl kataloogi nime konstant
-
-require_once CLASSES_DIR."template.php";
+define('CLASSES_DIR','classes/');
+define('TMPL_DIR','tmpl/');
+require_once CLASSES_DIR.'template.php';
 ?>
