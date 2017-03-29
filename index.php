f23b25ddd80cff57a5a4b40e8ddecbd707de3485
Martti <martti.merilain@khk.ee>
2017-03-29 12:45:22 +0300

    sisuelemendid

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
  * Time: 14:15
  */
 require_once 'conf.php';
-echo '<h1>Programmeerimise esileht</h1>';
-$main_tmpl = new template('main.html');
-//echo '<pre>';
-print_r($main_tmpl);
+echo'<h1>Esileht</h1>';
+//valmistame template objekti
+$main_tmpl = new template('template');
 $main_tmpl->set('user', 'Kasutajanimi');
 $main_tmpl->set('title', 'Pealeht');
 $main_tmpl->set('lang_bar', 'Keeleriba');
-$main_tmpl->set('menu','Peamenüü' );
-$main_tmpl->set('content', 'lehe sisu');
-$main_tmpl->set('site_title', 'Veebiprogemise kursus');
+$main_tmpl->set('menu', 'Menüü');
+$main_tmpl->set('content', 'Sisu');
+$main_tmpl->set('site_title', 'Veebi');
+//kontrollime objekti sisu
 echo $main_tmpl->parse();
-//echo '</pre>';
+//require_once 'menu.php';
 ?>
