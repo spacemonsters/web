fe2dab7866933ab4bd96afecaee0939e889cd98e
Martti <martti.merilain@khk.ee>
2017-04-05 10:45:16 +0300

    sisuelemendid:D

---
+++ menu.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martti
+ * Date: 5.04.2017
+ * Time: 10:45
+ */
+// loome menüü mallide objektid
+$menu = new template('menu.menu');
+$item = new template('menu.item');
+// lisame sisu
+// nimetame menüüs väljastav element
+$item->set('name', 'esimene');
+// loome antud menüü elemendile lingi
+$link = $http->getLink(array('act'=>'first'));
+// lisame antud link menüüsse
+$item->set('link', $link);
+// lisame valmis link menüü objekti sisse
+$menu->set('items', $item->parse());
+//
+$item->set('name', 'teine');
+$link = $http->getLink(array('act'=>'second'));
+$item->set('link', $link);
+$menu->add('items', $item->parse());
+// kontrollime objekti olemasolu ja sisu
+// kui soovime pidevat asendamist, siis set funktsioon add asemel
+$main_tmpl->add('menu', $menu->parse());
+?>
