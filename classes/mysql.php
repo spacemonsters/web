68ec295336da99c9486ee1ac31026ef88ff2af02
Martti <martti.merilain@khk.ee>
2017-04-05 11:35:30 +0300

    päring

--- classes/mysql.php
+++ classes/mysql.php
@@ -28,5 +28,15 @@ class mysql
             exit;
         }
     }// connect
+    //päringu teostamine
+    function query($sql){
+        $res=mysqli_query($this->conn, $sql);
+        if($res==false){
+        echo"Viga päringus!<br>";
+        echo"<b>".$sql."</b><br>";
+        echo mysqli_error($this->conn)."<br>";
+        exit;
+        }
+        return $res;
+    }//query
 } // klassi lõpp
-?>
