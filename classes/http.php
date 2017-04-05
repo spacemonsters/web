2e73463abfb9a2461f6ab299c99b0a36a4bca6d2
Martti <martti.merilain@khk.ee>
2017-04-05 10:43:33 +0300

    sisuelemendid:D

--- classes/http.php
+++ classes/http.php
@@ -7,17 +7,22 @@
  * Time: 22:17
  */
 class http
-{
-    var $vars = array();
-    var $server = array();
+{// klassi algus
+    // klassi muutujad
+    var $vars = array(); // http päringute andmed
+    var $server = array(); // serveri (masina) andmed
+    // klassi meetodid
+    // klassi konstruktor
     function __construct(){
         $this->init();
         $this->initCont();
     }
+    // paneme algandmed paika - initsialiseerime neid
     function init(){
         $this->vars = array_merge($_GET, $_POST, $_FILES);
         $this->server = $_SERVER;
-    }
+    }// init
+    //defineerime vajalikud konstandid
     function initCont(){
         $consts = array('REMOTE_ADDR', 'HTTP_HOST', 'PHP_SELF', 'SCRIPT_NAME');
         foreach ($consts as $const){
@@ -25,5 +30,21 @@ class http
                 define($const, $this->server[$const]);
             }
         }
-    }
-}
+    }// initConst
+    // saame kätte veebis olevad andmed - nagu $_POST või $_GET - emulatsioon
+    // tegelikult need andmed on kas lingi kaudu saadud
+    function get($name){
+        // kui vastava nimega element eksisteerib andmete massiivis
+        if($this->vars[$name]){
+            // tagastame selle väärtus
+            return $this->vars[$name];
+        }
+        // muidu tagastame tühi väärtus
+        return false;
+    }// get
+    // lisame vajalikud väärtused veebi kujul nimi=väärtus
+    function set($name, $val){
+        $this->vars[$name] = $val;
+    }// set
+}// klassi lõpp
+?>
