f0bf50be4b72b62ef34f95611ec0f657a35c8ee0
Martti <martti.merilain@khk.ee>
2017-04-05 10:50:27 +0300

    sisuelemendid:D

--- classes/template.php
+++ classes/template.php
@@ -7,63 +7,79 @@
  * Time: 12:11
  */
 class template
-{
-    var $file = ''; // html faili malli nimi
-    var $content = false; //html malli faili sisu
-    var $vars = array(); //html malli vaade sisu
-    //klassi konstruktor
-    function __construct($f)
-    {
-        $this->file = $f; // määrame html malli nime
-        $this->load_file();
-    }// __construct
-    //html malli faili lugemine
-    function load_file(){
-        $f = $this->file;
-        if (!is_dir(TMPL_DIR)){
-            echo 'Kataloogi '.TMPL_DIR.' ei leitud.<br>';
+{// klassi algus
+    // template klassi omadused - muutujad
+    var $file = ''; // html malli faili nimi
+    var $content = false; // html malli faili sisu
+    var $vars = array(); // html vaade sisu - reaalsed väärtused
+    // klassi tegevused - meetodid - funktsioonid
+    // klassi konstruktor
+    function __construct($f){
+        $this->file = $f; // määrame html malli faili nimi
+        $this->loadFile(); // loeme määratud failist sisu
+    }// konstruktor
+    // html malli faili lugemine
+    function loadFile(){
+        $f = $this->file; // lokaalne asendus
+        // kontrollime mallide kausta olemasolu
+        if(!is_dir(TMPL_DIR)){
+            echo 'Kataloogi '.TMPL_DIR.' ei ole leitud<br />';
             exit;
         }
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        // kui fail on olemas ja lugemiseks sobiv
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            // loeme failist malli sisu
+            $this->readFile($f);
         }
-        $f = TMPL_DIR.$this->file;
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        // lisame TMPL_DIR kasutusele
+        $f = TMPL_DIR.$this->file; // veel üks lokaalne asendus
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            // loeme failist malli sisu
+            $this->readFile($f);
         }
-        // .html laienduse eemaldamine
-        $f = TMPL_DIR.$this->file.'.html';
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        // lisame .html laienduse kasutusele
+        $f = TMPL_DIR.$this->file.'.html'; // veel üks lokaalne asendus
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            // loeme failist malli sisu
+            $this->readFile($f);
         }
-        $f = TMPL_DIR.str_replace('.','/', $this->file).'.html';
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        // lisame alamkataloogid kasutusele
+        $f = TMPL_DIR.str_replace('.', '/', $this->file).'.html'; // veel üks lokaalne asendus
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            // loeme failist malli sisu
+            $this->readFile($f);
         }
-        if ($this->content===false) {
-            echo 'Ei saanud sisu lugeda<br>';
+        // kui sisu ei olnud võimalik lugeda
+        if($this->content === false){
+            echo 'Ei suutnud lugeda faili '.$this->file.'<br />';
         }
-    }// load_file
-    function read_file($f){
+    }// loadFile
+    // loeme sisu html malli failist
+    function readFile($f){
         $this->content = file_get_contents($f);
-    }// read_file
-    // koostame elementide paarid
-    function set($name, $val) {
+    }// readFile
+    // koostame paarid malli_elemendi_nimi => reaalne_väärtus
+    function set($name, $val){
         $this->vars[$name] = $val;
     }// set
-    function add($name, $val) {
+    // lisame lisaväärtused olemasolevatele elementidele
+    function add($name, $val){
+        // kui antud nimega elementi ei eksisteeri
         if(!isset($this->vars[$name])){
-            $this->set($name,$val);
-        }
-        else {
-            $this->vars[$name] = $this->vars [$name] . $val;
+            $this->set($name, $val);
+        } else {
+            $this->vars[$name] = $this->vars[$name].$val;
         }
     }// add
-    function parse() {
-        $str = $this->content;
+    // html malli täitmine reaalse sisuga
+    function parse(){
+        $str = $this->content; // lokaalne asendus
+        // vaatame malli elementide massiivi
         foreach ($this->vars as $name=>$val){
-            $str=str_replace('{'.$name.'}', $val, $str);
+            $str = str_replace('{'.$name.'}', $val, $str);
         }
+        // tagastame täis täidetud malli sisu
         return $str;
     }// parse
-} // class end
+}// klassi lõpp
+?>
