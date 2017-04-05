4ff345d1bc26d8fb6446f889bcf3500d2d08e1ff
Martti <martti.merilain@khk.ee>
2017-04-05 08:38:45 +0300

    sisuelemendid

--- classes/template.php
+++ classes/template.php
@@ -8,63 +8,58 @@
  */
 class template
 {
-    var $file = ''; // html faili malli nimi
-    var $content = false; //html malli faili sisu
-    var $vars = array(); //html malli vaade sisu
-    //klassi konstruktor
-    function __construct($f)
-    {
-        $this->file = $f; // määrame html malli nime
-        $this->load_file();
+    var $file = '';
+    var $content = false;
+    var $vars = array();
+    function __construct($f){
+        $this->file=$f;
+        $this->loadFile();
     }
-    //html malli faili lugemine
-    function load_file(){
+    function loadFile(){
         $f = $this->file;
-        if (!is_dir(TMPL_DIR)){
-            echo 'Kataloogi '.TMPL_DIR.' ei leitud.<br>';
+        if(!is_dir(TMPL_DIR)){
+            echo 'Kataloogi '.TMPL_DIR.'ei ole leitud.<br>';
             exit;
         }
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
         }
         $f = TMPL_DIR.$this->file;
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
         }
-        // .html laienduse eemaldamine
         $f = TMPL_DIR.$this->file.'.html';
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
         }
-        $f = TMPL_DIR.str_replace('.','/', '$this->file').'.html';
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
-            $this->read_file($f);
+        $f = TMPL_DIR.str_replace('.','/', $this->file).'.html';
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
         }
-        if ($this->content===false) {
-            echo 'Ei saanud sisu lugeda<br>';
+        if ($this->content === false){
+            echo 'Ei suutnud lugeda failis '.$this->file.'<br>';
         }
     }
-    function read_file($f){
+    function readFile($f){
         $this->content = file_get_contents($f);
     }
-    // koostame elementide paarid
-    function set($name, $val) {
+    function set($name, $val){
         $this->vars[$name] = $val;
+    }// set
+    function add($name, $val){
+        if(!isset($this->vars[$name])){
+            $this->set($name, $val);
+        }else{
+            $this->vars[$name] = $this->vars[$name],$val;
+        }
     }
-    function parse() {
+    function parse(){
         $str = $this->content;
         foreach ($this->vars as $name=>$val){
-            $str=str_replace('{'.$name.'}', $val, $str);
+            $str = str_replace('{'.$name.'}', $val, $str);
         }
+        // return template content with real values
         return $str;
-    }
-}
-function($name, $val) {
-    if(!isset($this->vars[$name])){
-        $this->set($name,$val);
-    }
-    else {
-        $this->vars[$name] = $this->vars[$name].[$val];
-    }
+    }// parse
 }
 ?>
--- conf.php
+++ conf.php
@@ -5,9 +5,14 @@
  * Date: 15.03.2017
  * Time: 15:25
  */
-//defineerime vajalikud konstandid
+define('LIB_DIR', 'lib/');
 define('CLASSES_DIR', 'classes/'); //clasess kataloogi konstant
 define('TMPL_DIR', 'tmpl/'); //clasess kataloogi konstant
 //Failide kasutusele võtmine
+require_once LIB_DIR.'utils.php';
 require_once CLASSES_DIR.'template.php';
+require_once CLASSES_DIR.'http.php';
+require_once CLASSES_DIR.'linkobject.php';
+$http = new linkobject();
+echo $http->getlink(array('kasutaja'=>'nimi','pass'=>'parool'));
 ?>
