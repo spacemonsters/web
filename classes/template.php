9ea42aa045f280285539c1c3c73965bbfcb3e5c7
Martti <martti.merilain@khk.ee>
2017-04-05 08:50:31 +0300

    sisuelemendid:D

--- classes/template.php
+++ classes/template.php
@@ -8,57 +8,62 @@
  */
 class template
 {
-    var $file = '';
-    var $content = false;
-    var $vars = array();
-    function __construct($f){
-        $this->file=$f;
-        $this->loadFile();
-    }
-    function loadFile(){
+    var $file = ''; // html faili malli nimi
+    var $content = false; //html malli faili sisu
+    var $vars = array(); //html malli vaade sisu
+    //klassi konstruktor
+    function __construct($f)
+    {
+        $this->file = $f; // määrame html malli nime
+        $this->load_file();
+    }// __construct
+    //html malli faili lugemine
+    function load_file(){
         $f = $this->file;
-        if(!is_dir(TMPL_DIR)){
-            echo 'Kataloogi '.TMPL_DIR.'ei ole leitud.<br>';
+        if (!is_dir(TMPL_DIR)){
+            echo 'Kataloogi '.TMPL_DIR.' ei leitud.<br>';
             exit;
         }
-        if (file_exists($f) and is_file($f) and is_readable($f)){
-            $this->readFile($f);
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->read_file($f);
         }
         $f = TMPL_DIR.$this->file;
-        if (file_exists($f) and is_file($f) and is_readable($f)){
-            $this->readFile($f);
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->read_file($f);
         }
+        // .html laienduse eemaldamine
         $f = TMPL_DIR.$this->file.'.html';
-        if (file_exists($f) and is_file($f) and is_readable($f)){
-            $this->readFile($f);
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->read_file($f);
         }
         $f = TMPL_DIR.str_replace('.','/', $this->file).'.html';
-        if (file_exists($f) and is_file($f) and is_readable($f)){
-            $this->readFile($f);
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->read_file($f);
         }
-        if ($this->content === false){
-            echo 'Ei suutnud lugeda failis '.$this->file.'<br>';
+        if ($this->content===false) {
+            echo 'Ei saanud sisu lugeda<br>';
         }
-    }
-    function readFile($f){
+    }// load_file
+    function read_file($f){
         $this->content = file_get_contents($f);
-    }
-    function set($name, $val){
+    }// read_file
+    // koostame elementide paarid
+    function set($name, $val) {
         $this->vars[$name] = $val;
     }// set
-    function add($name, $val){
+    function add($name, $val) {
         if(!isset($this->vars[$name])){
-            $this->set($name, $val);
-        }else{
-            $this->vars[$name] = $this->vars[$name],$val;
+            $this->set($name,$val);
         }
-    }
-    function parse(){
+        else {
+            $this->vars[$name] = $this->vars [$name] . $val;
+        }
+    }// add
+    function parse() {
         $str = $this->content;
         foreach ($this->vars as $name=>$val){
-            $str = str_replace('{'.$name.'}', $val, $str);
+            $str=str_replace('{'.$name.'}', $val, $str);
         }
-        // return template content with real values
         return $str;
     }// parse
-}
+} // class end
