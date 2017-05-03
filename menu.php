59c73a3c078a3cceec6a4d637218bb97b7427828
Martti <martti.merilain@khk.ee>
2017-05-03 11:15:08 +0300

    päring

--- menu.php
+++ menu.php
@@ -9,26 +9,39 @@
 $menu = new template('menu.menu');
 $item = new template('menu.item');
 // lisame sisu
-$sql='select content_id,title from content where'.
-    'parent_id='.fixDb(0).'and show in_menu'.fixDb(1);
-$res=$db->getArray($sql);
+// menüü sql lause
+$sql = 'SELECT content_id, title FROM content WHERE '.
+    'parent_id='.fixDb(0).' AND show_in_menu='.fixDb(1);
+// saame päringu tulemus
+$res = $db->getArray($sql);
+// kontrollime tulemuse sisu
 if($res != false){
     foreach ($res as $page){
-        $item->set("name", $page["title"]);
-        $link=$http->getLink(array("page_id"=>$page["content_id"]));
-        $item->set("link", $link);
-        $item->add("items", $item->parse());
+        // nimetame menüüs väljastav element
+        $item->set('name', tr($page['title']));
+        // loome antud menüü elemendile lingi
+        $link = $http->getLink(array('page_id'=>$page['content_id']));
+        // lisame antud link menüüsse
+        $item->set('link', $link);
+        // lisame valmis link menüü objekti sisse
+        $menu->add('items', $item->parse());
     }
 }
-// nimetame menüüs väljastav element
-// loome antud menüü elemendile lingi
-// lisame antud link menüüsse
-// lisame valmis link menüü objekti sisse
-//
-$item->set('name', 'teine');
-$link = $http->getLink(array('act'=>'second'));
-$item->set('link', $link);
-$menu->add('items', $item->parse());
+// sisse logimine
+if(USER_ID == ROLE_NONE){
+    $item->set('name', tr('Logi sisse'));
+    $link = $http->getLink(array('act'=>'login'));
+    $item->set('link', $link);
+    $menu->add('items', $item->parse());
+}
+// välja logimine
+if(USER_ID != ROLE_NONE){
+    $item->set('name', tr('Logi välja'));
+    $link = $http->getLink(array('act'=>'logout'));
+    $item->set('link', $link);
+    $menu->add('items', $item->parse());
+}
 // kontrollime objekti olemasolu ja sisu
 // kui soovime pidevat asendamist, siis set funktsioon add asemel
 $main_tmpl->add('menu', $menu->parse());
+?>
