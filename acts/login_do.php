e984f72da494395f0d67511a4183cf0aafe78910
Martti <martti.merilain@khk.ee>
2017-05-03 11:13:05 +0300

    päring

---
+++ acts/login_do.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martti
+ * Date: 3.05.2017
+ * Time: 11:12
+ */
+$username = $http->get('kasutaja');
+$passwd = $http->get('parool');
+// koostame päringu kasutaja kontrollimiseks andmebaasis
+$sql = 'SELECT * FROM user '.
+    'WHERE username='.fixDb($username).
+    ' AND password='.fixDb(md5($passwd));
+$res = $db->getArray($sql);
+// teeme päringu tulemuse kontroll
+if($res == false){
+    // loome veateade ja paneme see sessiooni
+    $sess->set('error', 'Probleem sisselogimisega');
+    // siis tuleb suunata kasutaja sisselogimisvormi tagasi
+    $link = $http->getLink(array('act' => 'login'));
+    $http->redirect($link);
+} else {
+    // sisse tuleb avada kasutajale sessiooni
+    $sess->createSession($res[0]);
+    // tuleb suunata kasutajat pealehele
+    // kus ma väljastan kasutajaandmed sessiooni kontrolliks
+    $http->redirect();
+}
