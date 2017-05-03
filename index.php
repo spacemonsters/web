6175424d54f4c93926ee46dccc02dd629aefbff3
Martti <martti.merilain@khk.ee>
2017-05-03 11:15:45 +0300

    päring

--- index.php
+++ index.php
@@ -5,16 +5,25 @@
  * Date: 15.03.2017
  * Time: 14:15
  */
+// võtame konfiguratsiooni kasutusele
 require_once 'conf.php';
-echo'<h1>Esileht</h1>';
-//valmistame template objekti
-$main_tmpl = new template('template');
-$main_tmpl->set('user', 'Kasutajanimi');
-$main_tmpl->set('title', 'Pealeht');
-$main_tmpl->set('lang_bar', 'Keeleriba');
-$main_tmpl->set('menu', 'Menüü');
-$main_tmpl->set('content', 'Sisu');
-$main_tmpl->set('site_title', 'Veebi');
-//kontrollime objekti sisu
+// pealehe sisu
+echo '<h1>Veebiprogrammeerimise esileht</h1>';
+// valmistame peatemplate objekti
+$main_tmpl = new template('main');
+// valmistame paarid malli_element => väärtus
+$main_tmpl->set('user', $sess->user_data['username']);
+$main_tmpl->set('title', tr('Pealeht'));
+// lisame keeleriba pealehele
+require_once 'lang.php';
+$main_tmpl->set('menu', tr('Lehe peamenüü'));
+// kutsume menüü tööle testimiseks
+require_once 'menu.php';
+// tõstsime vaikimisi väljund default tegevuse sisse
+require_once 'act.php';
+$main_tmpl->set('site_title', 'Veebiprogrammeerimise kursus');
+// kontrollime antud objekti sisu
 echo $main_tmpl->parse();
-//require_once 'menu.php';
+// uuendame sessiooni andmed
+$sess->flush();
+?>
