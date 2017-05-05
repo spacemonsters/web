58a8efbd5e21e2cef41aa2d53dc7d0f9f4c79733
Martti <martti.merilain@khk.ee>
2017-05-05 11:39:26 +0300

    Uuendatud lehe vaikimisi selle sisu väljastamine

--- acts/default.php
+++ acts/default.php
@@ -5,5 +5,21 @@
  * Date: 5.04.2017
  * Time: 10:40
  */
-$main_tmpl->set('content', 'Lehe sisu');
+// lehe id saamine ja teisendamine - andmebaasis BIGINT
+$page_id = (int)$http->get('page_id');
+// sql lause lehe sisu otsimiseks vastavalt id-le
+$sql = 'SELECT * FROM content WHERE '.
+    'content_id='.fixDb($page_id);
+// saadame päring andmebaasi sisu saamiseks
+$res = $db->getArray($sql);
+if($res === false){
+    $sql = 'SELECT * FROM content WHERE '.
+        'is_first_page='.fixDb(1);
+    $res = $db->getArray($sql);
+}
+if($res !== false){
+    $page = $res[0];
+    $http->set('page_id', $page['content_id']);
+    $main_tmpl->set('content', $page['content']);
+}
 ?>
