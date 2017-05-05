3cd7ccf24a7fdab6a5eab6a5598e7fcf8556b4f2
spacemonsters <martti.merilain@khk.ee>
2017-05-05 14:07:56 +0300

    Parandustega

--- lang/lang_en.php
+++ lang/lang_en.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: erik
- * Date: 4/19/17
- * Time: 7:41 AM
- */
 $_trans = array(
     'Esimene leht' => 'First page',
     'Teine leht' => 'Second page',
