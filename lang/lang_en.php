20d69bbd77f16407812e894e4f6768d9507e56ab
Martti <martti.merilain@khk.ee>
2017-05-05 11:20:50 +0300

    Täiendatud lehe tõlge inglise keelde

---
+++ lang/lang_en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martti
+ * Date: 5.05.2017
+ * Time: 11:19
+ */
+$_trans = array(
+    'Esimene leht' => 'First page',
+    'Teine leht' => 'Second page',
+    'avaleht' => 'main page',
+    'Lehe peamenüü' => 'Page menu',
+    'eesti' => 'estonian',
+    'inglise' => 'english',
+    'vene' => 'russian',
+    'Kasutaja' => 'Username',
+    'Parool' => 'Password',
+    'Logi sisse' => 'Login',
+    'Logi välja' => 'Logout',
+    'Probleem sisselogimisega' => 'Problem with login'
+);
