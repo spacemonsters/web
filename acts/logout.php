ca3ea0037e2815ee1f6ea9ca6f326a416e3d493a
spacemonsters <martti.merilain@khk.ee>
2017-05-05 14:09:35 +0300

    Parandustega

--- acts/logout.php
+++ acts/logout.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: erkz
- * Date: 4.05.17
- * Time: 14:28
- */
 
 $sess->delSession(); // kustutame olemas olev sessioon
 $http->redirect();// suuname pealehele
