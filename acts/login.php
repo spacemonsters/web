d929a5ee25a2b3958b99511d7d95ef17764db94e
spacemonsters <martti.merilain@khk.ee>
2017-05-05 14:10:03 +0300

    Parandustega

--- acts/login.php
+++ acts/login.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: erkz
- * Date: 4.05.17
- * Time: 14:27
- */
 $login = new template('login');
 $error = $sess->get('error');
 $login->set('error', $error);
