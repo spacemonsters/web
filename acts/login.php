cfbae5d39476fe769a82d9021f17e01979f93163
Martti <martti.merilain@khk.ee>
2017-05-05 11:37:01 +0300

    Sisselogimine

--- acts/login.php
+++ acts/login.php
@@ -5,14 +5,15 @@
  * Date: 3.05.2017
  * Time: 11:11
  */
+// loome sisselogimisvormi objekti
 $login = new template('login');
 $error = $sess->get('error');
 $login->set('error', $error);
 // paneme reaalsed väärtused template elementidele
-$login->set('kasutajanimi', 'Kasutaja');
-$login->set('parool', 'Parool');
-$login->set('nupp', 'Logi sisse');
-// loome link sisselogimisvormi töötlusele
+$login->set('kasutajanimi', tr('Kasutaja'));
+$login->set('parool', tr('Parool'));
+$login->set('nupp', tr('Logi sisse'));
+// loome lingi sisselogimisvormi töötlusele
 $link = $http->getLink(array('act' => 'login_do'));
 $login->set('link', $link);
 // paneme sisu template sisse
