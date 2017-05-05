a6114ae90782ecd516937e93304b8da13403dba2
spacemonsters <martti.merilain@khk.ee>
2017-05-05 13:57:25 +0300

    Parandustega

--- menu.php
+++ menu.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: anna.karutina
- * Date: 22.03.2017
- * Time: 15:26
- */
 // loome menüü mallide objektid
 $menu = new template('menu.menu');
 $item = new template('menu.item');
