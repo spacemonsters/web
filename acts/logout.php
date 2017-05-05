2f867465779876844e8706b9ce799883adca45af
Martti <martti.merilain@khk.ee>
2017-05-05 11:33:30 +0300

    Lisatud kasutaja väljalogimine

--- acts/logout.php
+++ acts/logout.php
@@ -5,6 +5,6 @@
  * Date: 3.05.2017
  * Time: 11:13
  */
-$sess->delSession(); // kustutame olemas olev sessioon
+$sess->delSession(); // kustutame olemas oleva sessiooni
 $http->redirect();// suuname pealehele
 ?>
